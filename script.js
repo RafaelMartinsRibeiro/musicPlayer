a037d2dbc1dc0fa773d3f5c0a131c12f82fc514e
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-03-26 11:29:43 -0300

    Added play and pause function

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
-let trackList = [
-    {  
-        audio: "assets/music1.mp3",
-        image: "images/albumCover.jpg",
-        name: "Numb - Linkin Park",
-        artist: "Linkin Park"
-    },
-    {   
-        audioSrc: "assets/music2.mp3",
-        image: "a",
-        name: "a",
-        artist: "a"
+window.addEventListener("load", function(){
+    let trackList = [
+        {  
+            audioSrc: "assets/music1.mp3",
+            image: "images/albumCover1.jpg",
+            name: "Numb - Linkin Park",
+            artist: "Linkin Park"
+        },
+        {   
+            audioSrc: "assets/music2.mp3",
+            image: "images/albumCover2.png",
+            name: "Badolero pronde voi",
+            artist: "Torreto"
+        }
+    ];
+    
+    const audioControl = document.getElementById("audioControl");
+    let audio = document.getElementById("audio");
+    const image = document.getElementById("musicImage");
+    const musicName = document.getElementById("musicName");
+    const artistName = document.getElementById("artistName");
+    const progress = document.getElementById("progressed");
+    const currentTime = document.getElementById("currentTime");
+    const totalTime = document.getElementById("totalTime");
+    const playlist = document.getElementById("playlist");
+    const volume = document.getElementById("volume");
+    const currentVolume = document.getElementById("currentVolume");
+    const shuffle = document.getElementById("shuffle");
+    const rewind = document.getElementById("rewind");
+    const play = document.getElementById("play");
+    const forward = document.getElementById("forward");
+    const repeat = document.getElementById("repeat");
+
+
+    loadMusic(0);
+    let isPlaying = false; 
+
+    function loadMusic(trackIndex){
+        audio.src = trackList[trackIndex].audioSrc;
+        audioControl.load();
     }
-];
+    
+    function playVerify(){
+        isPlaying = true;
+        audioControl.play();
+        play.src = "images/pause.png"
+    }
 
-const audioControl = document.getElementById("audioControl");
-const audio = document.getElementById("audio");
-const image = document.getElementById("musicImage");
-const musicName = document.getElementById("musicName");
-const artistName = document.getElementById("artistName");
-const progress = document.getElementById("progressed");
-const currentTime = document.getElementById("currentTime");
-const totalTime = document.getElementById("totalTime");
-const playlist = document.getElementById("playlist");
-const volume = document.getElementById("volume");
-const currentVolume = document.getElementById("currentVolume");
-const shuffle = document.getElementById("shuffle");
-const rewind = document.getElementById("rewind");
-const play = document.getElementById("play");
-const forward = document.getElementById("forward");
-const repeat = document.getElementById("repeat");
+    function pauseVerify(){
+        isPlaying= false;
+        audioControl.pause();
+        play.src = "images/play.png"
+    }
 
-let audioIndex = 1;
+    function playMusic(){
+        if(isPlaying){
+            pauseVerify();
+        } else{
+            playVerify();
+        }
+    }
+
+    play.addEventListener("click", function(){
+        playMusic();
+    })
+
+    // function nextMusic(){
+    //     loadMusic(1)
+    //     playMusic();
+    // }
+    
+    // forward.addEventListener("click", function(){
+    //     nextMusic();
+    // })
+}) 
+
 
 
 
